300477bc40705a8bc337dd016fb2f2d0d715fd72
caetanoaronan <seu.email@exemplo.com>
2025-11-17 14:27:59 -0300

    feat(map): normalize module format in dashboard (M1 -> M01) and filter markers for filtered data
    - Added normalizeModule helper (accepts M1 or M01)
    - Use normalized module in search
    - Only create map markers for coordinates that match filtered dataset (updateMap)

---
+++ coordenadas_ppBio_fixed.js
@@ -0,0 +1,233 @@
+window.ppbioCoordenadas = [
+  {
+    "module": "M01",
+    "name": "M01T05",
+    "long": -49.65071931,
+    "lat": -28.15764911,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01T04",
+    "long": -49.6417543,
+    "lat": -28.15336888,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01T03",
+    "long": -49.63279004,
+    "lat": -28.14908809,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01T02",
+    "long": -49.6238265,
+    "lat": -28.14480674,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01T01",
+    "long": -49.6148637,
+    "lat": -28.14052481,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01T06",
+    "long": -49.61000822,
+    "lat": -28.14845756,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01T07",
+    "long": -49.61897147,
+    "lat": -28.1527398,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01T08",
+    "long": -49.62793544,
+    "lat": -28.15702149,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01T09",
+    "long": -49.63690015,
+    "lat": -28.16130261,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01T10",
+    "long": -49.64586558,
+    "lat": -28.16558317,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M01",
+    "name": "M01R02",
+    "long": -49.64544621,
+    "lat": -28.15513165,
+    "type": "Ripária"
+  },
+  {
+    "module": "M01",
+    "name": "M01R01",
+    "long": -49.62085783,
+    "lat": -28.14338879,
+    "type": "Ripária"
+  },
+  {
+    "module": "M01",
+    "name": "M01R05",
+    "long": -49.6062105,
+    "lat": -28.1451995,
+    "type": "Ripária"
+  },
+  {
+    "module": "M01",
+    "name": "M01R03",
+    "long": -49.60624506,
+    "lat": -28.14665941,
+    "type": "Ripária"
+  },
+  {
+    "module": "M01",
+    "name": "M01R04",
+    "long": -49.62206546,
+    "lat": -28.15422346,
+    "type": "Ripária"
+  },
+  {
+    "module": "M02",
+    "name": "M02T02",
+    "long": -49.503293,
+    "lat": -28.090199,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M02",
+    "name": "M02T06",
+    "long": -49.496336,
+    "lat": -28.079063,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M02",
+    "name": "M02T04",
+    "long": -49.497621,
+    "lat": -28.107533,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M02",
+    "name": "M02R01",
+    "long": -49.495494,
+    "lat": -28.114034,
+    "type": "Ripária"
+  },
+  {
+    "module": "M02",
+    "name": "M02T05",
+    "long": -49.494785,
+    "lat": -28.116201,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T01",
+    "long": -49.38598501,
+    "lat": -28.18363003,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T02",
+    "long": -49.39401667,
+    "lat": -28.17773333,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T03",
+    "long": -49.38645989,
+    "lat": -28.17458493,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T04",
+    "long": -49.40258333,
+    "lat": -28.18523333,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T05",
+    "long": -49.38915681,
+    "lat": -28.19226683,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T06",
+    "long": -49.38948304,
+    "lat": -28.15307244,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T07",
+    "long": -49.41484168,
+    "lat": -28.12335416,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T08",
+    "long": -49.40552023,
+    "lat": -28.17459036,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T09",
+    "long": -49.42098333,
+    "lat": -28.13781666,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03T10",
+    "long": -49.42465,
+    "lat": -28.16696667,
+    "type": "Terrestre"
+  },
+  {
+    "module": "M03",
+    "name": "M03R01",
+    "long": -49.38759685,
+    "lat": -28.17755066,
+    "type": "Ripária"
+  },
+  {
+    "module": "M03",
+    "name": "M03R02",
+    "long": -49.40934553,
+    "lat": -28.16980829,
+    "type": "Ripária"
+  },
+  {
+    "module": "M03",
+    "name": "M03R03",
+    "long": -49.40345968,
+    "lat": -28.182885,
+    "type": "Ripária"
+  }
+];
